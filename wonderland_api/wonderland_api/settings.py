c6630ddbe14e9eb7235c7028f11c1800bfd71fd2
Kelly <kellytan05925@gmail.com>
2025-04-07 17:54:33 -0400

    FrontEnd domain change

--- wonderland_api/wonderland_api/settings.py
+++ wonderland_api/wonderland_api/settings.py
@@ -135,5 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-  "https://myrabbithole.vercel.app"
+  "https://kellytan-portfolio.vercel.app"
 ]
