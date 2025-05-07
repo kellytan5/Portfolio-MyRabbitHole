18ba14c2bd35af1e2dcbc1031b766ed4232982a7
Kelly <kellytan05925@gmail.com>
2025-05-07 19:00:23 -0400

    Rasa URL

--- wonderland_api/wonderland/chatbot.py
+++ wonderland_api/wonderland/chatbot.py
@@ -3,8 +3,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-# RASA_URL = 'https://portfolio-myrabbithole.up.railway.app/webhooks/rest/webhook'
-RASA_URL = 'http://localhost:5005/webhooks/rest/webhook'
+RASA_URL = 'https://portfolio-myrabbithole.up.railway.app/webhooks/rest/webhook'
+# RASA_URL = 'http://localhost:5005/webhooks/rest/webhook'
 
 @csrf_exempt
 def chatbot_response(request):
