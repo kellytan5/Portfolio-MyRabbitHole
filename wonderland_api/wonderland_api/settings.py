7d00062f4565cddfa8150380c01efacf759a1b1f
Kelly <kellytan05925@gmail.com>
2025-07-11 19:54:21 -0400

    CORS origin update

--- wonderland_api/wonderland_api/settings.py
+++ wonderland_api/wonderland_api/settings.py
@@ -133,9 +133,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-  "http://localhost:8080",
-  "https://kellytan-portfolio.vercel.app"
+  "https://kellytan-portfolio.vercel.app",
+  "http://localhost:8080"
 ]
