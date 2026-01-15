797aa83ed06efd7c28e8ceb91ecf2105c227bc67
Kelly <kellytan05925@gmail.com>
2026-01-15 11:58:11 -0500

    Work experiences data: added company name

--- wonderland_api/wonderland/data/work_exp.py
+++ wonderland_api/wonderland/data/work_exp.py
@@ -1,19 +1,22 @@
-WORK_EXPERIENCES = [
+EXPERIENCES = [
   {
     'id': 1,
     'title': 'Software Engineering Co-op',
+    'company': 'CYA Live',
     'environment': 'Start-up Environment', 
     'overview': 'I worked within a small, cross-functional team where I gained exposure to real-world development workflows, testing practices, and iterative feature development. This experience taught me to understand how early-stage products evolve quickly and the importane of clear communication and adaptibility in a fast-paced environment.'
   },
   {
     'id': 2,
     'title': 'IT Support Co-op', 
+    'company': 'Centennial College',
     'environment': 'Post-Secondary Institution',
     'overview': 'I supported students and staff with general technical issues, strengthening my ability to communicate technical solutions clearly and document issues effectively. This role emphasized processes, user-focused problem-solving, and working within established IT procedures.'
   },
   {
     'id': 3,
     'title': 'Sales Associate', 
+    'company': 'Mr.Bin Building Supply Ltd.',
     'environment': 'Part-Time',
     'overview': 'Through customer-facing work, I developed strong communication and interpersonal skills, serving clients in English, Cantonese, and Mandarin. This experience reinforced the importance of understanding user needs and delivering clear, helpful interactions - skills I apply when designing and building user interfaces.'
   }
